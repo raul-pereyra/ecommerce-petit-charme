c4d48824892be3b6d20f37acd38c99f8f3938a7f
raul-pereyra <pereyra.rhp@gmail.com>
2021-02-11 00:13:32 -0300

    se elimino carpeta de clase

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import './App.scss';
 import NavbarComponent from './components/navbar/';
 import ButtonComponent from './components/button';
 import ItemListContainer from './containers/ItemListContainer';
-import ItemList from './components/clase5';
-import ItemWithState from './components/clase5';
+// import ItemList from './components/clase5';
 
 const App = () => {
   return (
@@ -11,10 +10,7 @@ const App = () => {
     <NavbarComponent  />
     <ItemListContainer greeting={'Bienvenido a Petit Charme, pronto encontrarás más productos.'}/>
     <ButtonComponent text={'agregar al carrito'}/>
-    {/* <ButtonComponentChildren text={'agregar al carrito'}> ESTE FORMATO SERIA CON UN CHILDREN
-      <div>Hola,soy el children del segundo boton</div>
-    </ButtonComponentChildren> */}
-    <ItemList/>
+    
     </>
   );
 }
