1c2693e7ce872c057fc1ec1be81fce93c4a35190
raul-laboral <pereyra.rhp@gmail.com>
2021-02-11 14:09:36 -0300

    se corrijio el problema en el ItemCount

--- src/components/itemCount/index.jsx
+++ src/components/itemCount/index.jsx
@@ -1,31 +1,35 @@
-import { useEffect} from "react";
-
+import { useEffect, useState} from "react";
 const ItemCount = () => {
-    let valorCont = 0;
-    let valorMas = () =>{
-        valorCont++;
-        console.log(valorCont)}
-    let valorMenos = () =>{
-        valorCont--;
-        console.log(valorCont)}
+    const [count, setCount] = useState(0);
+// const ItemCount = () => {
+//     let valorCont = 0;
+//     let valorMas = () =>{
+//         valorCont++;
+//         console.log(valorCont)}
+//     let valorMenos = () =>{
+//         valorCont--;
+//         console.log(valorCont)}
         
-        useEffect(() => {
-            // Consultas a la Base de Datos y suscripciones como AddEventList
-            console.log('se monto el componente');
-            return()=>{
-                // Desuscripciones de eventos o BD
-                console.log('se esta por morir');
-            }
-        }, [])/*Este es el filtro, si lo dejamos vacio se aplica cuando se monta. Ver hay varios filtros*/
-        // Se vuelve a renderizar, validaciones
-        console.log('se esta por renderizar');
+        // useEffect(() => {
+        //     // Consultas a la Base de Datos y suscripciones como AddEventList
+        //     console.log('se monto el componente');
+        //     return()=>{
+        //         // Desuscripciones de eventos o BD
+        //         console.log('se esta por morir');
+        //     }
+        // }, [])/*Este es el filtro, si lo dejamos vacio se aplica cuando se monta. Ver hay varios filtros*/
+        // // Se vuelve a renderizar, validaciones
+        // console.log('se esta por renderizar');
 
     return (
         <>
             <div className="cont__prod">
-                <button className='cont__menos btn__cont' onClick={valorMenos}>-</button>
+                {/* <button className='cont__menos btn__cont' onClick={valorMenos}>-</button>
                 <p>{valorCont}</p>
-                <button className='cont__mas btn__cont' onClick={valorMas}>+</button>
+                <button className='cont__mas btn__cont' onClick={valorMas}>+</button> */}
+                <button className='cont__menos btn__cont' onClick={() => setCount(count-1)}>-</button>
+                <p>{count}</p>
+                <button className='cont__mas btn__cont' onClick={() => setCount(count+1)}>+</button>
             </div>  
         </>
     )
